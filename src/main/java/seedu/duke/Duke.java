3121d620291c9281c291bf00a330e766fbf8811b
isaacsaw25 <isaacsaw25@gmail.com>
2024-09-28 22:13:36 +0800

    Add basic UI package

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -1,21 +1,30 @@
 package seedu.duke;
 
-import java.util.Scanner;
+import ui.UI;
 
 public class Duke {
     /**
      * Main entry-point for the java.duke.Duke application.
      */
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-        System.out.println("What is your name?");
+        uiTestRun();
+    }
 
-        Scanner in = new Scanner(System.in);
-        System.out.println("Hello " + in.nextLine());
+    /**
+     * Print greeting
+     * Prompt for user input then waits
+     * Echo user input
+     * Print sample ASCII table
+     * Print farewell message
+     */
+    public static void uiTestRun() {
+        UI.printGreeting();
+        UI.println("Type something...");
+        String input = UI.getUserInput();
+
+        UI.println("You typed: " + input);
+        UI.println("Anyways, here is the sample table: ");
+        UI.printTable(); // Prints sample table to console
+        UI.printBye();
     }
 }
